53edbd084bc8c72bb3969f9c99212196e0a43b92
NikaKolin <138161406+NikaKolin@users.noreply.github.com>
2025-03-31 12:36:43 -0400

    Add files via upload

---
+++ choropleth.py
@@ -0,0 +1,10 @@
+'''
+Assignment 7
+Name: Nika Kolin Melocoton
+Student ID: 211015210
+Class: CP321
+Date: 04-01-25
+Code Purpose: Creating py file to render the dash app
+'''
+import choropleth
+choropleth = choropleth.server
